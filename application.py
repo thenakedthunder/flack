31979de11fc18331031e78c8937cc53160b2c2f5
unknown <balazs.radvanyi88@gmail.com>
2019-06-25 07:32:42 +0200

    Implemented channel list view and channel creation

--- application.py
+++ application.py
@@ -1,13 +1,33 @@
 import os
 
 from flask import Flask, render_template
-#from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-#socketio = SocketIO(app)
+socketio = SocketIO(app)
+
+channels = []
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@socketio.on("channel creation")
+def create_channel(data):
+    channel_name = data["newChannelName"]
+
+    if (channel_name_taken(channel_name)):
+        emit("channel name taken", broadcast=True)
+        return
+
+    channels.append(channel_name)
+    emit("new channel created", {"channelName": channel_name}, broadcast=True)
+
+
+def channel_name_taken(channel_name):
+    if channel_name in channels:
+        return True;
+
+    return False;
