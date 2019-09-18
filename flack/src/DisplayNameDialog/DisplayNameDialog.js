15184d98426657de58706428464295df4af533ed
unknown <balazs.radvanyi88@gmail.com>
2019-09-18 20:23:22 +0200

    continued work on display name dialog

--- flack/src/DisplayNameDialog/DisplayNameDialog.js
+++ flack/src/DisplayNameDialog/DisplayNameDialog.js
@@ -1,6 +1,10 @@
+// -------------------- IMPORTS --------------------
+
+// react components
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Material UI components
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -9,26 +13,57 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
+// styling
 import './DisplayNameDialog.css';
 
 
 
 export default function DisplayNameDialog() {
+
+    // ---------------- STATE VARIABLES -----------------
+
     // this modal comes up right when the app is loaded
     const [open, setOpen] = React.useState(true);
+    // check if a display name is cached from the previous session and offer it
+    // as a displayname for the user by autofilling it in the form
+    const [displayName, setDisplayName] =
+        React.useState(localStorage.getItem("displayName"));
+    const [isBtnDisabled, setIsBtnDisabled] =       // for the submit button
+        React.useState(displayName ? false : true)
 
-    function handleClose(event) {
+
+    // --------------- HANDLER FUNCTIONS ----------------
+
+    const handleClose = event => {
+        localStorage.setItem("displayName", displayName);
         hideDialogWithAnimation();
 
         setTimeout(() => {
             setOpen(false);
-        }, 750);
+        }, 750);            // this gives time for the animation to end
     }
+
+    const handleChange = event => {
+        const newInput = event.target.value;
+
+        validate(newInput);
+
+        setDisplayName(newInput);
+
+        // displayName could not be used here, most likely because setting the 
+        // state works asynchronously
+        setIsBtnDisabled(newInput ? false : true);
+    }
+
+    // ---------------- HELPER FUNCTIONS ----------------
 
     const hideDialogWithAnimation = () => {
         const dialog = document.querySelector(".bounce-fade");
         dialog.classList.add("hide");
     }
+
+
+    // -------------- RENDERING COMPONENT ---------------
 
     return (
         <div>
@@ -42,6 +77,8 @@ export default function DisplayNameDialog() {
                     </DialogContentText>
                     <TextField
                         autoFocus
+                        onChange={handleChange}
+                        value={displayName}
                         margin="dense"
                         id="display-name"
                         label="Display Name"
@@ -50,7 +87,9 @@ export default function DisplayNameDialog() {
                     />
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose} color="primary">
+                    <Button onClick={handleClose}
+                            color="primary" 
+                            disabled={isBtnDisabled} >
                         Proceed with this name
                     </Button>
                 </DialogActions>
